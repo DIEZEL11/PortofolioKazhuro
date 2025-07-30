6e9733c8e00973b10324426693ed7e18b1bd5503
сергей кажуро <sergevm121@hotmail.com>
2025-07-30 22:02:25 +0300

    Добавьте файлы проекта.

---
+++ PortofolioKazhuro/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PortofolioKazhuro.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
